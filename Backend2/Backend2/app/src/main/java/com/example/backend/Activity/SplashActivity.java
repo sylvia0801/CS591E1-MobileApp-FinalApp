ec41c26ee5a4bf087e85fa292c3c8fa5027de686
YuwanXiao <42989502+YuwanXiao@users.noreply.github.com>
2020-04-21 16:36:53 -0400

    spash activity

--- Backend2/Backend2/app/src/main/java/com/example/backend/Activity/SplashActivity.java
+++ Backend2/Backend2/app/src/main/java/com/example/backend/Activity/SplashActivity.java
@@ -3,16 +3,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
-
 import com.example.backend.R;
-<<<<<<< HEAD:Backend2/Backend2/app/src/main/java/com/example/backend/Activity/Splash.java
-public class Splash extends AppCompatActivity {
-=======
-
 public class SplashActivity extends AppCompatActivity {
->>>>>>> 7aef56a2c4a235c9571609a46ebca1ba9a388f7a:Backend2/Backend2/app/src/main/java/com/example/backend/Activity/SplashActivity.java
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
