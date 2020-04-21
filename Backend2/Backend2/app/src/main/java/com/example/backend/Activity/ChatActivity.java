cd9b46343fea88c47889ad608357378a2cc22443
Cayden <cayden_ykj@163.com>
2020-04-21 19:39:42 -0400

    fix status

--- Backend2/Backend2/app/src/main/java/com/example/backend/Activity/ChatActivity.java
+++ Backend2/Backend2/app/src/main/java/com/example/backend/Activity/ChatActivity.java
@@ -5,8 +5,16 @@ import android.net.Uri;
 import android.os.Bundle;
 import com.example.backend.Fragment.buttom_fragment;
 import com.example.backend.R;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class ChatActivity extends AppCompatActivity implements buttom_fragment.OnFragmentInteractionListener {
+
+    private FirebaseUser firebaseUser;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +25,26 @@ public class ChatActivity extends AppCompatActivity implements buttom_fragment.O
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    private void status(String status){
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("status", status);
+
+        databaseReference.updateChildren(hashMap);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        status("online");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        status("offline");
     }
 }
